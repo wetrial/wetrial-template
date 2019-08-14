aa88bd96157e76eb938e6c55d97573fb2c969347
xiexingen <1002275364@qq.com>
2019-08-14 11:30:33 +0800

    demo

--- src/pages/Example/Drag-Drop/index.tsx
+++ src/pages/Example/Drag-Drop/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './index.less'
 
 class DashboardAnalysis extends React.Component {
   render() {
-    return <div>拖拽页面</div>;
+    return <div className={styles.iconGroup}>拖拽页面</div>;
   }
 }
 

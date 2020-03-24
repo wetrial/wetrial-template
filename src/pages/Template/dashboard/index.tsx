036485fd6e3b65102674361680c5301258af0352
xiexingen <1002275364@qq.com>
2020-03-24 17:26:31 +0800

    更新

--- src/pages/Template/dashboard/index.tsx
+++ src/pages/Template/dashboard/index.tsx
@@ -1,62 +1,28 @@
 import React from 'react';
-import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Card, Typography, Alert } from 'antd';
+import { useAccess, Access, useModel } from 'umi';
+import { Card, DatePicker, Button } from 'antd';
+import TemplatePermissions from '@config/modules/template';
 import styles from './index.less';
 
-const CodePreview: React.FC<{}> = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
-
-export default (): React.ReactNode => (
-  <PageHeaderWrapper>
-    <Card>
-      <Alert
-        message="umi ui 现已发布，点击右下角 umi 图标即可使用"
-        type="success"
-        showIcon
-        banner
-        style={{
-          margin: -12,
-          marginBottom: 24,
-        }}
-      />
-      <Typography.Text strong>
-        <a target="_blank" rel="noopener noreferrer" href="https://pro.ant.design/docs/block">
-          基于 block 开发，快速构建标准页面
-        </a>
-      </Typography.Text>
-      <CodePreview> npm run ui</CodePreview>
-      <Typography.Text
-        strong
-        style={{
-          marginBottom: 12,
-        }}
-      >
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://pro.ant.design/docs/available-script#npm-run-fetchblocks"
+export default (): React.ReactNode => {
+  const model = useModel('@@initialState');
+  const access = useAccess();
+  return (
+    <div className={styles.container}>
+      <Card>
+        <DatePicker />
+        <hr />
+        <Button
+          onClick={() => {
+            model.refresh();
+          }}
         >
-          获取全部区块
-        </a>
-      </Typography.Text>
-      <CodePreview> npm run fetch:blocks</CodePreview>
-    </Card>
-    <p
-      style={{
-        textAlign: 'center',
-        marginTop: 24,
-      }}
-    >
-      Want to add more pages? Please refer to{' '}
-      <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
-        use block
-      </a>
-      。
-    </p>
-  </PageHeaderWrapper>
-);
+          刷新权限
+        </Button>
+      </Card>
+      <Access accessible={access[TemplatePermissions.template.dashboard.index]} fallback="无权限">
+        有权限才能看到的信息
+      </Access>
+    </div>
+  );
+};

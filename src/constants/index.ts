804a94f87899f8d99de5e2c0ebdc2c31275532d5
xiexingen <1002275364@qq.com>
2020-08-05 13:51:53 +0800

    升级@wetrial/core @wetrial/component

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,18 +5,3 @@ export const BASE_PATH = '/';
 export const API_PREFIX = `${BASE_PATH}api/`;
 
 export const COMPANY_NAME = '湖南微试云';
-
-// 默认页码
-export const PAGE_SIZE = 10;
-
-// 分页属性
-export const PAGE_PROPS = {
-  defaultCurrent: 1,
-  total: 1,
-  pageSize: PAGE_SIZE,
-  defaultPageSize: PAGE_SIZE,
-  // showSizeChanger: true,
-  hideOnSinglePage: true,
-  // showQuickJumper:true,
-  showTotal: (total, [start, end]) => `第${start}-${end}条，共${total}条`,
-};

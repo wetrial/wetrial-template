6b8541a432b328983333f3aa8ca26d5703623583
xiexingen <1002275364@qq.com>
2020-11-17 19:56:34 +0800

    更新页面

--- src/pages/template/dashboard/index.tsx
+++ src/pages/template/dashboard/index.tsx
@@ -10,11 +10,11 @@ export default (): React.ReactNode => {
   const { refresh } = useModel('@@initialState');
 
   // 模拟切换用户角色
-  const handleToggleRole = (token: '00000' | '10000') => {
+  const handleToggleRole = async (token: '00000' | '10000') => {
     setToken({
       token,
     });
-    refresh();
+    await refresh();
   };
 
   return (
